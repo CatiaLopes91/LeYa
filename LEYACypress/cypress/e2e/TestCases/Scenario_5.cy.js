5e048a3693fe5831ce708ddfeec372fa698ec1c8
CatiaLopes <csousalopes1991@hotmail.com>
2025-01-18 17:01:03 +0000

    Scenario6

--- LEYACypress/cypress/e2e/TestCases/Scenario_5.cy.js
+++ LEYACypress/cypress/e2e/TestCases/Scenario_5.cy.js
@@ -1,15 +1,20 @@
 /// <reference types="cypress" />
-import { addListener } from 'process';
-import {searchBook, openBook} from '../GenericFunctions/genericFunctions'
+
+
 const website= require("../../../cypress.env.json")
-const locator= require("../Locators/locators.json")
+ 
 
 it.only('Validate the environment in dark mode" ', () => {
  /*  Change the background to dark mode. Check if the dark mode has been successfully applied to the website." */
  
  cy.visit(website.url);
- cy.get('link[data-role="darktheme"]').invoke('removeAttr', 'disabled');
+
+ cy.get('div[id="darkmode"]').should('be.visible').click();   
+ cy.get('body').should('have.css', 'background-color', 'rgb(30, 31, 30)'); 
+
+/* cy.get('link[data-role="darktheme"]').invoke('removeAttr', 'disabled');
  cy.get('link[data-role="darktheme"]').should('not.have.attr', 'disable');
  cy.get('body').should('have.css', 'background-color', 'rgb(30, 31, 30)'); 
+ */
 
 });
---
+++ LEYACypress/cypress/e2e/TestCases/Scenario_6.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+import { addListener } from 'process';
+import {searchBook, openBook} from '../GenericFunctions/genericFunctions'
+const website= require("../../../cypress.env.json")
+const locator= require("../Locators/locators.json")
+
+it.only('Validate the category of the books" ', () => {
+ /*  Validate the category about "A Guerra dos Pobres" it's equal to "Romance" */
+ 
+    cy.visit(website.url);
+    searchBook('A Guerra dos Pobres');
+    openBook('A Guerra dos Pobres');
+
+    cy.get('nav[aria-label="breadcrumb"]').should('be.visible').first();
+    cy.get('nav[aria-label="breadcrumb"]').find('ol').find('li').eq(2).contains('Romance');
+
+    
+ 
+
+});
