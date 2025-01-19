a37a669916155c8e6040a470bf9cf548b823eb3e
CatiaLopes <csousalopes1991@hotmail.com>
2025-01-19 02:24:16 +0000

    Scenario7

--- LEYACypress/cypress/e2e/GenericFunctions/genericFunctions.js
+++ LEYACypress/cypress/e2e/GenericFunctions/genericFunctions.js
@@ -1,9 +1,6 @@
 const selectors= require("../Locators/locators.json")
 
 const searchBook=(bookName) => {
-    
-   
-    
     cy.get(selectors.headerPage.searchInput).type(bookName);
     cy.wait(1000);
    // cy.get(selectors.headerPage.searchButton, { timeout: 20000 }).should('be.visible');
@@ -12,23 +9,31 @@ const searchBook=(bookName) => {
 }
 
 const openBook=(bookName) => {
-
-    cy.log('BOOK NAME-->');
-    cy.log(bookName);
-
-    /*cy.get("h6[class='book-title']").contains(bookName).should('be.visible');
-    cy.get("h6[class='book-title']").contains(bookName).click();*/
-   
-    
+  
     cy.get(selectors.bodyPage.bookTitle).contains(bookName).click();
     cy.get(selectors.bodyPage.bookSinopse).should('exist');
-    
-    
+
 }
 
+const openLateralMenu=(menuName) => {
+
+    cy.get(selectors.headerPage.menuBurger).should('exist').click();
+    cy.get("div[class='offcanvas-body']").contains(menuName).click();
+}
+
+const applyFilter=(filterName, filterValue) => {
+
+    cy.get(selectors.bodyPage.filterOption).should('exist').click();
+    
+    cy.get('div').contains(filterName).get('div').contains(filterValue).click();
+
+    cy.get(selectors.bodyPage.filterApplyed).contains(filterValue).should('exist')
+}
 
 export default{
     searchBook,
-    openBook
+    openBook,
+    openLateralMenu,
+    applyFilter
 
 };
---
+++ LEYACypress/cypress/e2e/TestCases/Scenario_7.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+import { addListener } from 'process';
+import {searchBook, openBook, openLateralMenu, applyFilter} from '../GenericFunctions/genericFunctions'
+const website= require("../../../cypress.env.json")
+const locator= require("../Locators/locators.json")
+
+it.only('Validate the filter price" ', () => {
+ /*  Validate the filter price about books. You need select Livros Menu option, add the filter < 10€. 
+ Validate if the first 10 books price is <10€" */
+ 
+    cy.visit(website.url);
+    
+    openLateralMenu('Livros');
+    applyFilter('Preço','<€10');
+    
+    cy.get("div[class*='book-card-container']").should('exist');
+    cy.get("div[class*='book-card-container']").find('.single-book-price').each(( $bookElement, index ) => {
+        
+        if (index <= 9) {
+            const bookPrice = $bookElement.find('h6').filter(':visible').clone().find('span.cut-price').remove().end().text().trim();
+            const bookFinalPriceCompare= bookPrice.replaceAll('€','').trim();
+            const final= parseFloat(bookFinalPriceCompare);
+            expect(final).to.be.lessThan(10);
+        }
+    });
+});
+
